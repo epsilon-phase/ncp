c22a5b0aea5fba410140dad2da8182b3c1ff412a
Violet white <violet.white.dammit@protonmail.com>
2020-04-04 15:28:29 -0500

    Add license file. Clean up incomplete files on signal termination

--- README.md
+++ README.md
@@ -1,18 +1,22 @@
 # ncp
 
-New CP. the venerable `cp` tool is powerful and well optimized(usually), but it's got a lot of semantics that
-don't usually align with our personal preferences. It can't provide progress reports. It works on individual files rather than directories.
+New CP.
+
+The venerable `cp` tool is powerful and well optimized(well, usually), but it's got a lot of semantics that
+don't usually align with our personal preferences. `cp` doesn't provide progress reports, `cp` works on individual files rather than directories.
+
+Worst of all, `cp` doesn't show a quickly updating number for maximum instant gratification.
 
 C++ makes it easy to replicate the parts we care about in
 what is likely far less code(looking at coreutils code is a penance
 when you don't care about the standard tool functionality)
 
 It cares about efficiency, it uses `mmap` when `sendfile`
-isn't available, but prefers that when possible because.
+isn't available, but prefers that when possible because it's faster.
 
 It wrongly assumes you know what you're doing and doesn't bother catching
 exceptions for permission issues and other such nonsense.
 
 We don't recommend it for you because this was born of a fit of pique.
 But if you think posix is encumbered with too much unix interop design
-and you hate unix, this... might be better?
+and you hate unix and think that robocopy is still too complicated, this... might be better?
--- src/main.cpp
+++ src/main.cpp
@@ -8,7 +8,9 @@
 #ifdef HAS_SENDFILE
 #include <sys/sendfile.h>
 #endif
+#include <cmath>
 #include <getopt.h>
+#include <signal.h>
 #include <sys/stat.h>
 #include <sys/types.h>
 #include <unistd.h>
@@ -19,13 +21,22 @@
     exit(EXIT_FAILURE);   \
   } while (0)
 namespace fs = std::filesystem;
+
 struct options {
   ssize_t chunk_size = std::numeric_limits<ssize_t>::max();
   bool print_info = true;
   bool copy_permissions = true;
+  /**
+   * Use ansi escapes to clear the line nicely :3
+   * */
+  bool ansi_escape = true;
+  bool is_copying = false;
   const char *original_dir = nullptr,
              *destination_dir = nullptr;
+  fs::path current_dest;
 };
+static volatile bool is_killed = false;
+void handle_termination(int sigterm);
 void print_help(const char *progname);
 void mmap_copy(int original, int newfile, const struct stat &stat, const options &opts);
 #ifdef HAS_SENDFILE
@@ -33,18 +44,19 @@ void sendfile_copy(int original, int newfile, const struct stat &stat, const opt
 #endif
 options parse_arguments(int argc, char **argv);
 void validate_options(const options &);
+void print_progress(ssize_t copied, ssize_t total, const options &);
+void handle_death(int, int, const options &);
 int main(int argc, char **argv) {
   auto options = parse_arguments(argc, argv);
   validate_options(options);
-  if (!fs::is_directory(options.original_dir)) {
-    std::cout << options.original_dir << " is not a directory" << std::endl;
-    return 1;
-  }
-
+  struct sigaction action;
+  memset(&action, 0, sizeof(action));
+  action.sa_handler = &handle_termination;
+  sigaction(SIGTERM, &action, NULL);
   fs::path other = options.destination_dir,
            src = options.original_dir;
+  std::cout << "Currently working on:" << std::endl;
   for (auto i : fs::recursive_directory_iterator(src, fs::directory_options::skip_permission_denied)) {
-
     fs::path newpath = other;
     fs::path::iterator a = src.begin(), b = i.path().begin();
     auto original_perms = fs::status(i.path()).permissions();
@@ -62,7 +74,8 @@ int main(int argc, char **argv) {
       if (options.copy_permissions)
         fs::permissions(newpath, original_perms);
     } else if (i.is_regular_file() && !fs::exists(newpath)) {
-      std::cout << fs::absolute(i.path()) << "->" << fs::absolute(newpath) << std::endl;
+      if (options.print_info)
+        std::cout << "\x1b[1A\x1b[1000D\x1b[0K" << fs::absolute(i.path()) << "->" << fs::absolute(newpath) << std::endl;
       int original = open(fs::absolute(i.path()).c_str(), O_RDONLY);
       if (original == -1) {
         handle_error("ORIGINAL FILE");
@@ -72,22 +85,28 @@ int main(int argc, char **argv) {
         handle_error("FSTAT");
       }
       int newfile = open(fs::absolute(newpath).c_str(), O_CREAT | O_RDWR);
+      options.current_dest = newpath;
       if (newfile == -1) {
         handle_error("NEWFILE");
       }
+      //Doing this permits running 'rm' on the resulting file, even if the program aborts prior to
+      //finishing the copy
+      if (options.copy_permissions)
+        fs::permissions(newpath, original_perms);
       //File should exist by this point,
       //but sendfile and mmap don't like size 0 files
       if (stat.st_size > 0) {
+        options.is_copying = true;
 #ifdef HAS_SENDFILE
         sendfile_copy(original, newfile, stat, options);
 #else
         mmap_copy(original, newfile, stat, options);
 #endif
+        options.is_copying = false;
       }
+      handle_death(original, newfile, options);
       close(original);
       close(newfile);
-      if (options.copy_permissions)
-        fs::permissions(newpath, original_perms);
     }
   }
 
@@ -102,10 +121,22 @@ void mmap_copy(int original, int newfile, const struct stat &stat, const options
     handle_error("mmap ORIGINAL");
   }
   char *new_data;
+  char *offset_out = new_data,
+       *offset_in = original_data;
   new_data = (char *)mmap(NULL, stat.st_size, PROT_WRITE | PROT_READ, MAP_SHARED, newfile, 0);
   if (new_data == MAP_FAILED)
     handle_error("mmap NEW");
-  memcpy(new_data, original_data, stat.st_size);
+  ssize_t copied = 0,
+          chunk_size;
+  while (copied < stat.st_size) {
+    chunk_size = std::min(stat.st_size - copied, options.chunk_size);
+    memcpy(offset_out, offset_in, chunk_size);
+    copied += chunk_size;
+    offset_in += chunk_size;
+    offset_out += chunk_size;
+    print_progress(copied, stat.st_size, options);
+    handle_death(original, newfile, options);
+  }
   munmap(new_data, stat.st_size);
   munmap(original_data, stat.st_size);
 }
@@ -113,15 +144,14 @@ void mmap_copy(int original, int newfile, const struct stat &stat, const options
 void sendfile_copy(int original, int newfile, const struct stat &stat, const options &options) {
   off_t copied = 0;
   ftruncate(newfile, stat.st_size);
+  int chunks = 0;
   while (copied < stat.st_size) {
     if (-1 == sendfile(newfile, original, &copied, std::min(options.chunk_size, stat.st_size - copied))) {
       handle_error("SENDFILE");
     }
-    if (options.print_info)
-      std::cout << "Sent " << copied / std::min(0x7ffff000, options.chunk_size) << " chunks" << '\r' << std::flush;
+    print_progress(copied, stat.st_size, options);
+    handle_death(original, newfile, options);
   }
-  if (options.print_info)
-    std::cout << std::endl;
 }
 #endif
 options parse_arguments(int argc, char **argv) {
@@ -151,7 +181,24 @@ options parse_arguments(int argc, char **argv) {
       print_help(argv[0]);
       break;
     case 'c': {
-      auto q = strtol(optarg, NULL, 10);
+      char *end = nullptr;
+      auto q = strtol(optarg, &end, 10);
+      if (end != nullptr) {
+        //fallthrough is good actually :3
+        switch (*end) {
+        case 'g':
+        case 'G':
+          q *= 1024;
+        case 'm':
+        case 'M':
+          q *= 1024;
+        case 'k':
+        case 'K':
+          q *= 1024;
+          break;
+        }
+      }
+
       result.chunk_size = q;
     } break;
     }
@@ -173,14 +220,14 @@ void validate_options(const options &opts) {
   if (opts.original_dir == nullptr) {
     std::cerr << "Origin directory not specified" << std::endl;
     invalid = true;
-  } else if (fs::is_directory(opts.original_dir)) {
+  } else if (!fs::is_directory(opts.original_dir)) {
     std::cerr << "Origin directory '" << opts.original_dir << "' does not exist" << std::endl;
     invalid = true;
   }
   if (opts.destination_dir == nullptr) {
     std::cerr << "Destination directory not specified" << std::endl;
     invalid = true;
-  } else if (fs::is_directory(opts.destination_dir)) {
+  } else if (!fs::is_directory(opts.destination_dir)) {
     std::cerr << "Destination directory '" << opts.destination_dir << "' does not exist" << std::endl;
     invalid = true;
   }
@@ -197,8 +244,31 @@ void print_help(const char *argv) {
             << "\t--disregard-permissions\t Do not copy permissions. This is usually not what you want.\n"
             << "\t--silent\t Don't print anything\n"
             << "\t--loud\t On by default. Print things\n"
-            << "\t--chunk-size <size>\t Set the amount copied at once defaults to " << std::numeric_limits<ssize_t>::max() << "\n"
+            << "\t--chunk-size <size>\t Set the amount copied at once defaults to " << std::numeric_limits<ssize_t>::max() << " bytes.\n"
+            << "\t\t Add a suffix of k,m, or g to specify the unit,\n"
+            << "\t\t since it's not fun to remember the exact size of each of those units\n"
             << "\t--preserve-permissions\tCopy the original permissions from each file and directory\n"
             << std::endl;
   exit(0);
+}
+void print_progress(ssize_t copied, ssize_t total, const options &options) {
+  if (!options.print_info)
+    return;
+  double progress = copied;
+  progress /= total;
+  progress *= 100.0;
+  std::cout << "\x1b[1000D\x1b[0K" << options.current_dest << std::setw(8) << std::setprecision(5) << progress << "%" << std::flush;
+}
+void handle_termination(int sigterm) {
+  is_killed = true;
+}
+void handle_death(int original_fd, int new_fd, const options &options) {
+  if (!is_killed)
+    return;
+  if (options.is_copying) {
+    close(original_fd);
+    close(new_fd);
+    fs::remove(options.current_dest);
+  }
+  exit(1);
 }
